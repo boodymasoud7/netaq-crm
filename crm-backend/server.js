52c9adc823d71b8c579e7d9244eda3b236eb8656
boodymasoud7 <boodymasoud7@gmail.com>
2025-10-02 00:02:57 +0300

    Fix: Add www.netaqcrm.site to CORS allowed origins

--- crm-backend/server.js
+++ crm-backend/server.js
@@ -50,7 +50,7 @@ const limiter = rateLimit({
 app.use(helmet());
 app.use(limiter);
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173', 'http://localhost:4173', process.env.FRONTEND_URL].filter(Boolean),
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173', 'http://localhost:4173', 'https://netaqcrm.site', 'https://www.netaqcrm.site', process.env.FRONTEND_URL].filter(Boolean),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Dev-User-Id'],
