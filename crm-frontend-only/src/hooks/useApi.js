d2fe067b360300a3fa13379fe62e369aa394bf0d
boodymasoud7 <boodymasoud7@gmail.com>
2025-12-03 01:33:36 +0200

    fix: Add checkLeadDuplicates to useApi hook and window.api

--- crm-frontend-only/src/hooks/useApi.js
+++ crm-frontend-only/src/hooks/useApi.js
@@ -10,7 +10,7 @@ export const useApi = () => {
   const makeApiCall = async (apiFunction, ...args) => {
     setLoading(true)
     setError(null)
-    
+
     try {
       const result = await apiFunction(...args)
       setLoading(false)
@@ -36,7 +36,7 @@ export const useApi = () => {
     getCurrentUser: () => makeApiCall(authAPI.getCurrentUser),
     updateProfile: (data) => makeApiCall(authAPI.updateProfile, data),
     changePassword: (oldPass, newPass) => makeApiCall(authAPI.changePassword, oldPass, newPass),
-    
+
     // Clients methods
     getClients: (params) => makeApiCall(dbAPI.getClients, params),
     getClientById: (id) => makeApiCall(dbAPI.getClientById, id),
@@ -44,53 +44,53 @@ export const useApi = () => {
     updateClient: (id, data) => makeApiCall(dbAPI.updateClient, id, data),
     deleteClient: (id) => makeApiCall(dbAPI.deleteClient, id),
     getClientStats: () => makeApiCall(dbAPI.getClientStats),
-    
+
     // Archive methods
     getArchivedClients: (params) => makeApiCall(dbAPI.getArchivedClients, params),
     restoreClient: (id) => makeApiCall(dbAPI.restoreClient, id),
     permanentDeleteClient: (id) => makeApiCall(dbAPI.permanentDeleteClient, id),
     permanentDeleteAllClients: () => makeApiCall(dbAPI.permanentDeleteAllClients),
-    
+
     getArchivedLeads: (params) => makeApiCall(dbAPI.getArchivedLeads, params),
     restoreLead: (id) => makeApiCall(dbAPI.restoreLead, id),
     permanentDeleteLead: (id) => makeApiCall(dbAPI.permanentDeleteLead, id),
     permanentDeleteAllLeads: () => makeApiCall(dbAPI.permanentDeleteAllLeads),
-    
+
     getArchivedSales: (params) => makeApiCall(dbAPI.getArchivedSales, params),
     restoreSale: (id) => makeApiCall(dbAPI.restoreSale, id),
     permanentDeleteSale: (id) => makeApiCall(dbAPI.permanentDeleteSale, id),
-    
+
     getArchivedProjects: (params) => makeApiCall(dbAPI.getArchivedProjects, params),
     restoreProject: (id) => makeApiCall(dbAPI.restoreProject, id),
     permanentDeleteProject: (id) => makeApiCall(dbAPI.permanentDeleteProject, id),
-    
+
     getArchivedUnits: (params) => makeApiCall(dbAPI.getArchivedUnits, params),
     restoreUnit: (id) => makeApiCall(dbAPI.restoreUnit, id),
     permanentDeleteUnit: (id) => makeApiCall(dbAPI.permanentDeleteUnit, id),
-    
+
     getArchivedDevelopers: (params) => makeApiCall(dbAPI.getArchivedDevelopers, params),
     restoreDeveloper: (id) => makeApiCall(dbAPI.restoreDeveloper, id),
     permanentDeleteDeveloper: (id) => makeApiCall(dbAPI.permanentDeleteDeveloper, id),
     permanentDeleteAllDevelopers: () => makeApiCall(dbAPI.permanentDeleteAllDevelopers),
-    
+
     // Follow-ups Archive methods
     getArchivedFollowUps: (params) => makeApiCall(dbAPI.getArchivedFollowUps, params),
     restoreFollowUp: (id) => makeApiCall(dbAPI.restoreFollowUp, id),
     permanentDeleteFollowUp: (id) => makeApiCall(dbAPI.permanentDeleteFollowUp, id),
     permanentDeleteAllFollowUps: () => makeApiCall(dbAPI.permanentDeleteAllFollowUps),
-    
+
     // Tasks Archive methods
     getArchivedTasks: (params) => makeApiCall(dbAPI.getArchivedTasks, params),
     restoreTask: (id) => makeApiCall(dbAPI.restoreTask, id),
     permanentDeleteTask: (id) => makeApiCall(dbAPI.permanentDeleteTask, id),
     permanentDeleteAllTasks: () => makeApiCall(dbAPI.permanentDeleteAllTasks),
-    
+
     // Reminders Archive methods
     getArchivedReminders: (params) => makeApiCall(dbAPI.getArchivedReminders, params),
     restoreReminder: (id) => makeApiCall(dbAPI.restoreReminder, id),
     permanentDeleteReminder: (id) => makeApiCall(dbAPI.permanentDeleteReminder, id),
     permanentDeleteAllReminders: () => makeApiCall(dbAPI.permanentDeleteAllReminders),
-    
+
     // Leads methods
     getLeads: (params) => makeApiCall(dbAPI.getLeads, params),
     getLeadById: (id) => makeApiCall(dbAPI.getLeadById, id),
@@ -99,7 +99,8 @@ export const useApi = () => {
     deleteLead: (id) => makeApiCall(dbAPI.deleteLead, id),
     convertLeadToClient: (id) => makeApiCall(dbAPI.convertLeadToClient, id),
     getLeadStats: () => makeApiCall(dbAPI.getLeadStats),
-    
+    checkLeadDuplicates: (phone, email, excludeId) => makeApiCall(dbAPI.checkLeadDuplicates, phone, email, excludeId),
+
     // Projects methods
     getProjects: (params) => makeApiCall(dbAPI.getProjects, params),
     getProjectById: (id) => makeApiCall(dbAPI.getProjectById, id),
@@ -107,7 +108,7 @@ export const useApi = () => {
     updateProject: (id, data) => makeApiCall(dbAPI.updateProject, id, data),
     deleteProject: (id) => makeApiCall(dbAPI.deleteProject, id),
     getProjectStats: () => makeApiCall(dbAPI.getProjectStats),
-    
+
     // Sales methods
     getSales: (params) => makeApiCall(dbAPI.getSales, params),
     getSaleById: (id) => makeApiCall(dbAPI.getSaleById, id),
@@ -115,7 +116,7 @@ export const useApi = () => {
     updateSale: (id, data) => makeApiCall(dbAPI.updateSale, id, data),
     deleteSale: (id) => makeApiCall(dbAPI.deleteSale, id),
     getSalesStats: (params) => makeApiCall(dbAPI.getSalesStats, params),
-    
+
     // Tasks methods
     getTasks: (params) => makeApiCall(dbAPI.getTasks, params),
     getMyTasks: (params) => makeApiCall(dbAPI.getMyTasks, params),
@@ -124,25 +125,25 @@ export const useApi = () => {
     updateTask: (id, data) => makeApiCall(dbAPI.updateTask, id, data),
     deleteTask: (id) => makeApiCall(dbAPI.deleteTask, id),
     getTaskStats: (params) => makeApiCall(dbAPI.getTaskStats, params),
-    
+
     // Notes methods
     getNotes: (itemType, itemId, params = {}) => makeApiCall(dbAPI.getNotes, itemType, itemId, params),
     addNote: (data) => makeApiCall(dbAPI.addNote, data),
     updateNote: (id, data) => makeApiCall(dbAPI.updateNote, id, data),
     deleteNote: (id) => makeApiCall(dbAPI.deleteNote, id),
-    
+
     // User Notifications methods
     getUserNotifications: (params) => makeApiCall(dbAPI.getUserNotifications, params),
     getUnreadNotificationsCount: () => makeApiCall(dbAPI.getUnreadNotificationsCount),
     markNotificationsAsRead: (notificationIds) => makeApiCall(dbAPI.markNotificationsAsRead, notificationIds),
     clearAllNotifications: () => makeApiCall(dbAPI.clearAllNotifications),
-    
+
     // Interactions methods
     getInteractions: (params) => makeApiCall(dbAPI.getInteractions, params),
     addInteraction: (data) => makeApiCall(dbAPI.addInteraction, data),
     updateInteraction: (id, data) => makeApiCall(dbAPI.updateInteraction, id, data),
     deleteInteraction: (id) => makeApiCall(dbAPI.deleteInteraction, id),
-    
+
     // Follow-ups methods
     getFollowUps: (params) => makeApiCall(dbAPI.getFollowUps, params),
     getFollowUpById: (id) => makeApiCall(dbAPI.followUpsAPI.getFollowUpById, id),
@@ -209,7 +210,7 @@ export const useApi = () => {
     updateUser: (id, data) => makeApiCall(dbAPI.updateUser, id, data),
     deleteUser: (id) => makeApiCall(dbAPI.deleteUser, id),
     updateUserStatus: (id, status) => makeApiCall(dbAPI.updateUserStatus, id, status),
-    
+
     // Combined stats
     getStats: () => makeApiCall(dbAPI.getStats),
     getManagerStats: () => makeApiCall(dbAPI.getManagerStats),
@@ -226,7 +227,7 @@ export const useApiData = (apiFunction, dependencies = []) => {
   const fetchData = async () => {
     setLoading(true)
     setError(null)
-    
+
     try {
       const result = await apiFunction()
       setData(result)
@@ -261,7 +262,7 @@ export const usePaginatedApi = (apiFunction, initialParams = {}) => {
   const fetchData = async (newParams = params) => {
     setLoading(true)
     setError(null)
-    
+
     try {
       const result = await apiFunction(newParams)
       setData(result.data || [])
